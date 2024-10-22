53702848867861732b0c42fc20ea61ee07803158
AdvitG <garg.advit@gmail.com>
2024-10-22 17:21:46 +0530

    doneeee

--- 1q217/1q217.cpp
+++ 1q217/1q217.cpp
@@ -1,21 +1,16 @@
 #include <iostream> 
 #include <vector>
+#include <unordered_set>
 using namespace std; 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        vector<int> comp; 
-        comp.push_back(nums[0]); 
-        for (int i = 1; i < nums.size(); i++) {
-            //cout<<"in";  
-            for(int j=0; j < comp.size();j++)
-                //cout<<"in"; 
-                if(comp[j]==nums[i]){
-                    return true; 
-                }
-            comp.push_back(nums[i]); 
-            
-            
+        unordered_set<int> seen; 
+        for (int a : nums) {
+            if (seen.find(a) != seen.end()) {
+                return true;
+            }
+            seen.insert(a);
         }
         return false; 
     }
